0b7d6107c4d9d6a593a909e99ea80671e8347872
Samuel Adesoga <samuel.adesoga@HMCTS.NET>
2017-05-06 15:57:50 +0100

    work in progress

--- lib/pokerwars/hand.ex
+++ lib/pokerwars/hand.ex
@@ -5,5 +5,23 @@ defmodule Pokerwars.Hand do
     evaluate(cards)
   end
 
-  defp evaluate(_), do: :high_card
+  defp evaluate(cards) do
+  	cond do
+  		flush?(cards) -> :royal_flush
+  		true -> :high_card
+  	end 
+  end
+
+  defp flush?(cards) do
+  	suits = Enum.map(cards, fn(%{rank: rank, suit: suit}) -> suit end)
+  	length(Enum.dedup(suits)) == 1
+  end
+
+  defp sort_by_rank(cards) do
+    Enum.sort(cards)
+  end
+
+  defp sort_by_suit(cards) do
+    Enum.sort(cards, fn(x,y) -> x.suit < y.suit end)
+  end
 end
